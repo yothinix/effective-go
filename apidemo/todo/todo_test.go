75e1360aa6f9ee75d5d8d6d2eabd861654176877
Yothin Muangsommuk <m@yothinix.com>
2022-01-10 22:38:26 +0700

    Modify test to be independent from 3rd party

--- apidemo/todo/todo_test.go
+++ apidemo/todo/todo_test.go
@@ -1,31 +1,47 @@
 package todo
 
 import (
-	"bytes"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 func TestCreateTodoNotAllowSleepTask(t *testing.T) {
-	handler := NewTodoHandler(&gorm.DB{})
-
-	w := httptest.NewRecorder()
-	payload := bytes.NewBufferString(`{"text": "sleep"}`)
-	req, _ := http.NewRequest("POST", "http://0.0.0.0:8080/todos", payload)
-	req.Header.Add("TransactionID", "testIDxxx")
-
-	c, _ := gin.CreateTestContext(w)
-	c.Request = req
+	handler := NewTodoHandler(&TestDB{})
+	c := &TestContext{}
 
 	handler.NewTask(c)
 
-	want := `{"error":"not allowed"}`
+	want := "not allowed"
 
-	if want != w.Body.String() {
-		t.Errorf("want %s but get %s\n", want, w.Body.String())
+	if want != c.v["error"] {
+		t.Errorf("want %s but get %s\n", want, c.v["error"])
 	}
+}
+
+type TestDB struct{}
+
+func (TestDB) New(*Todo) error {
+	return nil
+}
+
+type TestContext struct{
+	v map[string]interface{}
+}
+
+func (TestContext) Bind(v interface{}) error {
+	*v.(*Todo) = Todo{
+		Title: "sleep",
+	}
+	return nil
+}
+
+func (c *TestContext) JSON(code int, v interface{}) {
+	c.v = v.(map[string]interface{})
+}
+
+func (TestContext) TransactionID() string {
+	return "TestTransactionID"
+}
+
+func (TestContext) Audience() string {
+	return "Unit Test"
 }
